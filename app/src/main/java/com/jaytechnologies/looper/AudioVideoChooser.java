8dd034087c0cf3a1d534e51cb21e044e31cf5b1d
Jared Glaser <jmglaser@udel.edu>
2019-01-17 23:11:27 -0500

    added FFMPEG

--- app/src/main/java/com/jaytechnologies/looper/AudioVideoChooser.java
+++ app/src/main/java/com/jaytechnologies/looper/AudioVideoChooser.java
@@ -9,6 +9,15 @@ import android.os.Bundle;
 import android.widget.Button;
 import android.view.View;
 import 	android.content.Intent;
+import android.widget.TextView;
+
+import com.github.hiteshsondhi88.libffmpeg.ExecuteBinaryResponseHandler;
+import com.github.hiteshsondhi88.libffmpeg.FFmpeg;
+import com.github.hiteshsondhi88.libffmpeg.LoadBinaryResponseHandler;
+import com.github.hiteshsondhi88.libffmpeg.exceptions.FFmpegCommandAlreadyRunningException;
+import com.github.hiteshsondhi88.libffmpeg.exceptions.FFmpegNotSupportedException;
+
+import org.w3c.dom.Text;
 
 
 public class AudioVideoChooser extends AppCompatActivity {
@@ -20,7 +29,56 @@ public class AudioVideoChooser extends AppCompatActivity {
         Button audioChoice = findViewById(R.id.audioBtn);
         Button videoChoice = findViewById(R.id.videoBtn);
 
+        //FFmpeg stuff. only run once.
+        FFmpeg ffmpeg = FFmpeg.getInstance(getApplicationContext());
+        try {
+            ffmpeg.loadBinary(new LoadBinaryResponseHandler() {
 
+                @Override
+                public void onStart() {}
+
+                @Override
+                public void onFailure() {}
+
+                @Override
+                public void onSuccess() {}
+
+                @Override
+                public void onFinish() {}
+            });
+        } catch (FFmpegNotSupportedException e) {
+            // Handle if FFmpeg is not supported by device
+        }
+
+        /*//test
+        ffmpeg = FFmpeg.getInstance(getApplicationContext());
+        try {
+            // to execute "ffmpeg -version" command you just need to pass "-version"
+            ffmpeg.execute(new String[]{"-version"}, new ExecuteBinaryResponseHandler() {
+
+                @Override
+                public void onStart() {
+
+                }
+                @Override
+                public void onProgress(String message) {}
+
+                @Override
+                public void onFailure(String message) {}
+
+                @Override
+                public void onSuccess(String message) {
+
+                }
+                @Override
+                public void onFinish() {}
+            });
+
+
+        } catch (FFmpegCommandAlreadyRunningException e) {
+            // Handle if FFmpeg is already running
+        }
+*/
         //TODO: BAD BAD BAD implementation... if they say no then we crash
         if (ContextCompat.checkSelfPermission(getApplicationContext(),
                 Manifest.permission.READ_EXTERNAL_STORAGE)
